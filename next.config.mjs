915cd7fb0851015856356b2c5ba604b69d832286
HenryKun55 <henriqueflavio55@gmail.com>
2025-05-17 17:44:47 -0300

    back again basePath -,- holy sh.

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,8 @@ const nextConfig = {
   images: {
     unoptimized: true, // Necessário para exportação estática
   },
-  basePath: '',
+  basePath: process.env.NODE_ENV === 'production' ? '/ponto' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/ponto/' : '',
 };
 
 export default nextConfig;
